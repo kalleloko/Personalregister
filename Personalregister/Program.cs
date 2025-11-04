ede822d358b1026ede3f87e8288e138653216696
Kalle Lindqvist <kallelin@gmail.com>
2025-11-04 15:08:18 +0100

    Add console menu

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Personalregister
+﻿
+namespace Personalregister
 {
     internal class Program
     {
+
+        private Dictionary<int, string> menu = new Dictionary<int, string>()
+        {
+            {1, "Lägg till anställd"},
+            {2, "Visa alla anställda"},
+            // {3, "Ta bort anställd"},
+            // {4, "Sök anställd"},
+            // {5, "Avsluta"}
+        };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hej! Vad vill du göra idag?");
+            PrintMenu();
+        }
+
+        private static void PrintMenu()
+        {
+            foreach (var item in new Program().menu)
+            {
+                Console.WriteLine($"{item.Key}. {item.Value}");
+            }
         }
     }
 }
