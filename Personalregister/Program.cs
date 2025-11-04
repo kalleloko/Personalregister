0032f97f84e533cf91da87b03bd1bb9e60b9bf1f
Kalle Lindqvist <kallelin@gmail.com>
2025-11-04 15:24:04 +0100

    Add console menu interaction

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 namespace Personalregister
 {
     internal class Program
@@ -15,7 +16,33 @@ namespace Personalregister
         static void Main(string[] args)
         {
             Console.WriteLine("Hej! Vad vill du göra idag?");
-            PrintMenu();
+            bool doRun = true;
+            while (doRun)
+            {
+                PrintMenu();
+                int input = GetInput();
+                if(input == 0)
+                {
+                    Console.WriteLine("Tack och hej!");
+                    doRun = false;
+                } else
+                {
+                    Console.WriteLine($"Du valde: {new Program().menu[input]}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// get user input and validate it
+        /// </summary>
+        /// <returns>an int that corresponds to a menu item</returns>
+        private static int GetInput()
+        {
+            if(!int.TryParse(Console.ReadLine(), out int input))
+            {
+                return 0;
+            }
+            return new Program().menu.ContainsKey(input) ? input : 0;
         }
 
         private static void PrintMenu()
